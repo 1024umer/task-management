b8ed0dba01e7a91a06fb3416172624ba3a727a9f
1024umer <127780703+1024umer@users.noreply.github.com>
2023-11-09 04:17:04 +0500

    login logout register api

---
+++ app/Http/Controllers/Auth/ApiAuthController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class ApiAuthController extends Controller
+{
+    public function login(Request $request){
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:8',
+        ]);
+        if ($validator->fails()) {
+            return response(['errors' => $validator->errors()->all()], 422);
+        }
+        $user = User::where('email', $request->email)->first();
+        if ($user) {
+            if ($user->is_email_verified == 1) {
+                if (Hash::check($request->password, $user->password)) {
+                    $token = $user->createToken('SternGym Token Grant')->accessToken;
+                    $response = ['token' => $token, 'user' => $user];
+                    return response($response, 200);
+                } else {
+                    $response = ["message" => "Password mismatch"];
+                    return response($response, 422);
+                }
+            } else {
+                if (Hash::check($request->password, $user->password)) {
+                    $token = $user->createToken('SternGym Token Grant')->accessToken;
+                    $response = ['token' => $token, 'user' => $user];
+                    return response($response, 200);
+                }
+                $response = ["message" => "Password doesn't match"];
+                return response($response, 422);
+            }
+        } else {
+            $response = ["message" => 'User does not exist'];
+            return response($response, 422);
+        }
+    }
+    public function register(RegisterRequest $request){
+        $user = User::create([
+            'email' => $request->email,
+            'name' => $request->name,
+            'username' => $request->username,
+            'phone' => $request->phone,
+            'country_id' =>$request->country_id,
+            'password' => Hash::make($request->password),
+            'role_id' => $request->role_id?2:3,
+        ]);
+        $token = $user->createToken('Task Management Token Grant')->accessToken;
+        $user_detail = User::where('id', $user->id)->first();
+        $response = ['token' => $token, 'user' => $user_detail];
+        return response($response, 200);
+    }
+    public function logout(Request $request){
+        $token = $request->user()->token();
+        $token->revoke();
+        $response = ['message' => 'You have been successfully logged out!'];
+        return response($response, 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\Auth\ApiAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', [ApiAuthController::class, 'register']);
+Route::post('/login', [ApiAuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:api']], function () {
+	Route::post('/logout', [ApiAuthController::class, 'logout']);
+
 });
