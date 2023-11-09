8fa8004909fbfef1e985fe15bbef9b93444499d7
1024umer <127780703+1024umer@users.noreply.github.com>
2023-11-09 22:37:15 +0500

    file and task schema

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\ApiAuthController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 /*
@@ -13,11 +14,12 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::group(['middleware' => ['cors', 'json.response']], function () {
+	Route::post('/register', [ApiAuthController::class, 'register']);
+	Route::post('/login', [ApiAuthController::class, 'login']);
+});
 
-Route::post('/register', [ApiAuthController::class, 'register']);
-Route::post('/login', [ApiAuthController::class, 'login']);
-
-Route::group(['middleware' => ['auth:api']], function () {
+Route::group(['middleware' => ['cors', 'json.response','auth:api']], function () {
 	Route::post('/logout', [ApiAuthController::class, 'logout']);
-
+	Route::apiResource('/task',TaskController::class);
 });
