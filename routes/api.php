442ff07ad4db5a5e5edd962519bb79ef64fafc66
1024umer <127780703+1024umer@users.noreply.github.com>
2023-11-30 01:38:37 +0500

    fixes in api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\Auth\{ApiAuthController,ForgetPasswordController};
-use App\Http\Controllers\{CountryController,WorkController,StatsController,ProfileController,TaskController,ProposalController,EducationController,EmployemnetController};
-use App\Http\Controllers\Admin\{AdminEducationController,SkillController,UserController,RoleController,AdminTaskController,AdminProposalController};
+use App\Http\Controllers\Auth\{ApiAuthController, ForgetPasswordController};
+use App\Http\Controllers\{CountryController, WorkController, StatsController, ProfileController, TaskController, ProposalController, EducationController, EmployemnetController};
+use App\Http\Controllers\Admin\{AdminEducationController, SkillController, UserController, RoleController, AdminTaskController, AdminProposalController};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 /*
@@ -15,49 +15,50 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
 Route::group(['middleware' => ['cors', 'json.response']], function () {
 	Route::post('/register', [ApiAuthController::class, 'register']);
 	Route::post('/login', [ApiAuthController::class, 'login']);
 	Route::post('/admin-login', [ApiAuthController::class, 'adminLogin']);
-	Route::apiResource('country',CountryController::class)->only(['index']);
+	Route::apiResource('country', CountryController::class)->only(['index']);
 });
 
-Route::group(['middleware' => ['cors', 'json.response','auth:api']], function () {
+Route::group(['middleware' => ['cors', 'json.response', 'auth:api']], function () {
 	Route::post('/logout', [ApiAuthController::class, 'logout']);
-	Route::apiResource('task',TaskController::class);
-	Route::get('most-recent',[TaskController::class,'mostRecent']);
-	Route::get('best-match',[TaskController::class,'bestMatch']);
-	Route::get('all-task',[TaskController::class,'all']);
-	Route::get('progress-task',[TaskController::class,'progress']);
-	Route::get('completed-task',[TaskController::class,'completed']);
-	Route::post('create-proposal/{id}',[ProposalController::class,'createProposal']);
-	Route::apiResource('employement',EmployemnetController::class);
-	Route::apiResource('education',EducationController::class);
-	Route::apiResource('profile',ProfileController::class);
-	Route::apiResource('stats',StatsController::class)->only(['index']);
-	Route::apiResource('work',WorkController::class);
-	Route::post('review/{$id}',[WorkController::class,'inReview']);
-	Route::post('pending/{$id}',[WorkController::class,'inPending']);
-	Route::post('completed/{$id}',[WorkController::class,'inCompleted']);
+	Route::apiResource('task', TaskController::class);
+	Route::get('most-recent', [TaskController::class, 'mostRecent']);
+	Route::get('best-match', [TaskController::class, 'bestMatch']);
+	Route::get('all-task', [TaskController::class, 'all']);
+	Route::get('progress-task', [TaskController::class, 'progress']);
+	Route::get('completed-task', [TaskController::class, 'completed']);
+	Route::post('create-proposal/{id}', [ProposalController::class, 'createProposal']);
+	Route::apiResource('employement', EmployemnetController::class);
+	Route::apiResource('education', EducationController::class);
+	Route::apiResource('profile', ProfileController::class);
+	Route::apiResource('stats', StatsController::class)->only(['index']);
+	Route::apiResource('work', WorkController::class);
+	Route::post('review/{$id}', [WorkController::class, 'inReview']);
+	Route::post('pending/{$id}', [WorkController::class, 'inPending']);
+	Route::post('completed/{$id}', [WorkController::class, 'inCompleted']);
 	//Admin
-	Route::apiResource('skill',SkillController::class);
-	Route::apiResource('admin-task',AdminTaskController::class);
-	Route::apiResource('admin-education',AdminEducationController::class);
-	Route::apiResource('admin-proposal',AdminProposalController::class);
-	Route::apiResource('users',UserController::class);
-	Route::apiResource('roles',RoleController::class);
+	Route::apiResource('skill', SkillController::class);
+	Route::apiResource('admin-task', AdminTaskController::class);
+	Route::apiResource('admin-education', AdminEducationController::class);
+	Route::apiResource('admin-proposal', AdminProposalController::class);
+	Route::apiResource('users', UserController::class);
+	Route::apiResource('roles', RoleController::class);
 });
 
 Route::group(['middleware' => ['auth:api']], function () {
-    Route::get('/me', function (Request $request) {
-        return $request->user();
-    });
+	Route::get('/me', function (Request $request) {
+		return $request->user();
+	});
 });
 
 //Forget Password Starts here
 
 // Route::get('/forget-password', [ForgetPasswordController::class, 'index'])->name('auth.forgetPassword');
 Route::post('/otp-send', [ForgetPasswordController::class, 'otpSend'])->middleware('guest');
-Route::post('/otp-verify',[ForgetPasswordController::class,'reset'])->middleware('guest');
+Route::post('/otp-verify', [ForgetPasswordController::class, 'reset'])->middleware('guest');
 Route::post('/reset-password', [ForgetPasswordController::class, 'resetPassword'])->middleware('guest');
 //Forget Password Ends here
